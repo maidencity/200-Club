bf56efda7ac522d48073fb660018f6a57abcc46c
Stephen Cox <andronics@gmail.com>
2020-08-05 00:14:05 +0100

    initial build

---
+++ precache-manifest.a9cfa7c68633bc88a100968e4a1293ee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "decbfbb0a3d32e71160790d462e47448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f61c7683624bbacc7e1",
+    "url": "/static/css/2.87e83766.chunk.css"
+  },
+  {
+    "revision": "bab46227cce076cb6f5d",
+    "url": "/static/css/main.0b873d5a.chunk.css"
+  },
+  {
+    "revision": "3f61c7683624bbacc7e1",
+    "url": "/static/js/2.b714b8ad.chunk.js"
+  },
+  {
+    "revision": "300a0a01a49a6ce568c9b658dbd7888d",
+    "url": "/static/js/2.b714b8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab46227cce076cb6f5d",
+    "url": "/static/js/main.85aafc49.chunk.js"
+  },
+  {
+    "revision": "3cf8e2cfd79590b2f404",
+    "url": "/static/js/runtime-main.97c0833e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3cbde4b8ea395818548125158cf977e3",
+    "url": "/static/media/logo.3cbde4b8.svg"
+  }
+]);
